cded939d54283ba0135e2af55ccc02b0a5fefe8d
RigleyC <rigley2010@hotmail.com>
2016-11-26 21:27:55 -0200

    Update app.js

--- app.js
+++ app.js
@@ -15,4 +15,39 @@ app.get('/', function(req, res) {
     });
 });
 
-module.exports = app;
+function persistentMenu(sender){
+ request({
+    url: 'https://graph.facebook.com/v2.6/me/thread_settings',
+    qs: {access_token:token},
+    method: 'POST',
+    json:{
+        setting_type : "call_to_actions",
+        thread_state : "existing_thread",
+        call_to_actions:[
+            {
+              type:"postback",
+              title:"FAQ",
+              payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
+            },
+            {
+              type:"postback",
+              title:"I Prodotti in offerta",
+              payload:"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
+            },
+            {
+              type:"web_url",
+              title:"View Website",
+              url:"https://google.com/"
+            }
+          ]
+    }
+
+}, function(error, response, body) {
+    console.log(response)
+    if (error) {
+        console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error)
+    }
+})
+}
