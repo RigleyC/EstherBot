0e45b6f938139895a2d93e3e7306ddb68bdaa8b2
RigleyC <rigley2010@hotmail.com>
2016-11-27 16:20:55 -0200

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,35 +1,36 @@
-curl https://api.smooch.io/v1/appusers/c7f6e6d6c3a637261bd9656f/messages \
-     -X POST \
-     -H 'content-type: application/json' \
-     -H 'authorization: Bearer your-jwt' \
-     -d '
-{
-    "role": "appMaker",
-    "items": [{
-        "title": "Tacos",
-        "description": "Description",
-        "mediaUrl": "http://example.org/image.jpg",
-        "actions": [{
-            "text": "Select",
-            "type": "postback",
-            "payload": "TACOS"
-        }, {
-            "text": "More info",
-            "type": "link",
-            "uri": "http://example.org"
-        }]
-    }, {
-        "title": "Ramen",
-        "description": "Description",
-        "mediaUrl": "http://example.org/image.jpg",
-        "actions": [{
-            "text": "Select",
-            "type": "postback",
-            "payload": "RAMEN"
-        }, {
-            "text": "More info",
-            "type": "link",
-            "uri": "http://example.org"
-        }]
-    }]
-}
+curl -X POST -H "Content-Type: application/json" -d '{
+  "setting_type":"call_to_actions",
+  "thread_state":"new_thread",
+  "call_to_actions":[
+    {
+      "message":{
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"generic",
+            "elements":[
+              {
+                "title":"Привет, мы тестим бота!",
+                "item_url":"https://vc.ru",
+                "image_url":"https://scontent-amt2-1.xx.fbcdn.net/hphotos-xat1/v/t1.0-9/12439361_483333288528724_5142446386712463547_n.png?oh=1bbbf86c6c0b992bff95dd72d459ae13&oe=577A3A38",
+                "subtitle":"Нажмите на кнопки или пишите в чат",
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "title":"View Website",
+                    "url":"https://vc.ru"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"Bot command",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      }
+    }
+  ]
+}' "https://graph.facebook.com/v2.6/483332995195420/thread_settings?access_token=7m371lmqx8fd6ovd1ilypplg3"
