7c3969918bdb978e4c74c9cd36b8112800cc3e96
RigleyC <rigley2010@hotmail.com>
2016-11-27 14:40:17 -0200

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -81,6 +81,27 @@ if (process.env.SERVICE_URL) {
 app.post('/webhook', function(req, res, next) {
     var isPostback = req.body.trigger == "postback";
     var msg = '';
+//---------------------------------------------
+let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			if (text === 'Generic') {
+				sendGenericMessage(sender)
+				continue
+			}
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		}
+		if (event.postback) {
+			let text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			continue
+		}
+	}
+	res.sendStatus(200)
+})
 	
 //---------------------------------------------	
 function sendGenericMessage(sender) {
