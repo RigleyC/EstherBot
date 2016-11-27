8f5b4a792e7216b67d69484225a350abf8428a4e
RigleyC <rigley2010@hotmail.com>
2016-11-27 17:48:57 -0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-
+//---------------------------
+const request = require('request')
+//---------------------------
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 //---------------------------
 app.use(bodyParser.urlencoded({extended: false}))
+
 //---------------------------
 app.get('/', function(req, res) {
     res.render('index', {
