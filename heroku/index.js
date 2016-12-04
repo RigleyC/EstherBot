2678af8d8c0cc8fb2ecdf95729e323333c08c71a
RigleyC <rigley2010@hotmail.com>
2016-12-04 17:21:06 -0200

    step5

--- heroku/index.js
+++ heroku/index.js
@@ -163,7 +163,7 @@ if (event.message && event.message.text) {
 			let text = event.message.text
 			if (text === 'Generic') {
 				sendGenericMessage(sender)
-				continue
+			//	continue
 			}
 		//	sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
             
