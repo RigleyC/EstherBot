03df679fdba64c5e0ab4041f1b5f7e3da304e6f7
RigleyC <rigley2010@hotmail.com>
2016-12-05 00:22:55 -0200

    test?

--- heroku/index.js
+++ heroku/index.js
@@ -209,24 +209,96 @@ function kittenMessage(recipientId, text) {
         });
 });
 //----------------------------------------------------------------
-/*
-if (event.message && event.message.text) {
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
 			let text = event.message.text
 			if (text === 'Generic') {
 				sendGenericMessage(sender)
-			//	continue
+				continue
 			}
-		//	sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))   
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
 			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
 			continue
 		}
-	//}
+	}
 	res.sendStatus(200)
-    });
-    */
+})
+
+
+// recommended to inject access tokens as environmental variables, e.g.
+// const token = process.env.PAGE_ACCESS_TOKEN
+
+function sendTextMessage(sender, text) {
+	let messageData = { text:text }
+	
+	request({
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+function sendGenericMessage(sender) {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
 //----------------------------------------------------------------
 var server = app.listen(process.env.PORT || 8000, function() {
     var host = server.address().address;
