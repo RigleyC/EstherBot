50d58cce3541c40d5a9fc369fb7ae15f5b1fd29a
RigleyC <rigley2010@hotmail.com>
2016-12-04 23:57:38 -0200

    ujhyv

--- heroku/index.js
+++ heroku/index.js
@@ -92,7 +92,7 @@ app.use(bodyParser.json());
 //-------------------------------------------------------------------------------------
 app.post('/webhook', function(req, res, next) {
 //-------------------------------------------------------------------------------------
- var events = req.body.entry[0].messaging;
+ //var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
