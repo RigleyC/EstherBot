a5a9ab4ea64ff6db38a5bc9bd0b4a107fa0c7320
RigleyC <rigley2010@hotmail.com>
2016-12-05 00:08:26 -0200

    and now?

--- heroku/index.js
+++ heroku/index.js
@@ -92,17 +92,18 @@ app.use(bodyParser.json());
 //-------------------------------------------------------------------------------------
 app.post('/webhook', function(req, res, next) {
 //-------------------------------------------------------------------------------------
- //var events = req.body.entry[0].messaging;
-    for (var i = 0; i < events.length; i++) {
+ /*var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
+            */
             if (!kittenMessage(event.sender.id, event.message.text)) {
                 sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
             }
-        } else if (event.postback) {
+         else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
         }
-    }
+    
     res.sendStatus(200);
 //-------------------------------------------------------------------------------------    
     var isPostback = req.body.trigger == "postback";
