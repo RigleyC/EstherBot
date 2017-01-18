caae31ac16bdcbae498a25dc51e023cf5de316e7
RigleyC <rigley2010@hotmail.com>
2017-01-18 15:06:40 -0200

    testando 211-227

--- heroku/index.js
+++ heroku/index.js
@@ -209,7 +209,21 @@ function kittenMessage(recipientId, text) {
         });
 });
 //----------------------------------------------------------------
+const botBuilder = require('claudia-bot-builder');
+const fbTemplate = botBuilder.fbTemplate;
 
+module.exports = botBuilder(message => {
+  if (message.type === 'facebook') {
+    const newMessage = new fbTemplate.Text('What\'s your favorite House in Game Of Thrones');
+
+    return newMessage
+      .addQuickReply('Stark', 'STARK')
+      .addQuickReply('Lannister', 'LANNISTER')
+      .addQuickReply('Targaryen', 'TARGARYEN')
+      .addQuickReply('None of the above', 'OTHER')
+      .get();
+  }
+});
 //----------------------------------------------------------------
 var server = app.listen(process.env.PORT || 8000, function() {
     var host = server.address().address;
